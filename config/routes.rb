9613dd2a5f0d36667849666e8e14d2fc815f72f5
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2016-11-10 19:42:55 +0000

    before beautifier

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome', to: 'welcome#index'
-  get 'about', to: 'welcome#about'
+  resources :posts
+
+  get 'about' => 'welcome#about'
   root 'welcome#index'
 end
