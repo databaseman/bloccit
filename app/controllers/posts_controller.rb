6b81ed6c44c93a4bf8e30122a05f7c24ef22d799
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2016-11-15 23:53:30 +0000

    crudCheckPoint before beautify

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,30 @@
+#
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all   # give me all rows from table Posts
   end
 
   def show
+     @post = Post.find(params[:id])      
   end
 
   def new
+    @post = Post.new
   end
 
+   def create
+     @post = Post.new
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
+     if @post.save  # Calling database save/insert command
+       flash[:notice] = "Post was saved."
+       redirect_to @post #Redirecting to @post will direct the user to the posts show view.
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :new  # render the new view again
+     end
+   end
+   
   def edit
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+#
+# resources :posts    <= will automatically generates all the necessary routes
+#                        for posts
+# If you want to generate only one, you can use
+#   resources :posts only: :update
+# If you want to generate more than one, you can use
+#   resources :posts only: [:edit, :update]
+#
 Rails.application.routes.draw do
   resources :posts
 
