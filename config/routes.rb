8077591562487bd35a42a799e8a72910dff212cd
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2016-11-29 02:01:39 +0000

    topicsPostsAssignment after generate controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@
 #   resources :posts only: [:edit, :update]
 #
 Rails.application.routes.draw do
+  get 'sponsored_post/show'
+
+  get 'sponsored_post/new'
+
+  get 'sponsored_post/edit'
+
   resources :topics do
     resources :posts, except: [:index]
   end
