d3cce94abade2d95d966ac2917f3232f8b027d50
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2016-12-19 22:29:49 +0000

    profileAssignment 1

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def user_has_posts?
+    current_user.posts.any?
+  end
+  def user_has_comments?
+    current_user.comments.any?
+  end  
+  def user_has_favorites?
+    current_user.favorites.any?
+  end  
 end
